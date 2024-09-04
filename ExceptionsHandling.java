c3a0b457e92a17b072b3d1978f812b5a3f60a2d2
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-04 11:11:30 +0200

    handled arithmetic exception

--- ExceptionsHandling.java
+++ ExceptionsHandling.java
@@ -2,6 +2,19 @@
 public class ExceptionsHandling {
 
     public static void main(String[] args) {
-        System.err.println(args[0]);
+        /* 
+        int d = 0;
+        int a = 42 / d;
+        System.err.println(a);
+         */
+
+        try {
+            int d = 0;
+            int a = 42 / d;
+            System.out.println("This will not be printed");
+        } catch (ArithmeticException e) {
+            System.err.println(e);
+            System.err.println("Division by zero");
+        }
     }
 }
